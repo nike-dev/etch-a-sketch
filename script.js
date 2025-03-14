eed739c9eda1992e0e44f211b1f7fc30d9e7ca78
nike-dev <146763214+nike-dev@users.noreply.github.com>
2025-03-14 22:44:33 +0200

    Add prompt button for grid size
    The program now asks user for the grid size.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const container = document.querySelector("#container");
-
-function createGrid(rows, columns) {
-    for (let j = 0; j < columns; j++) {
+const promptBtn = document.createElement("button");
+promptBtn.textContent = "Start";
+container.append(promptBtn);
+function createGrid(e) {
+    const squaresPerSide = prompt("What size grid you want?");
+    for (let j = 0; j < squaresPerSide; j++) {
         const rowDiv = document.createElement("div");
+        rowDiv.setAttribute("class", "rowDiv");
         container.append(rowDiv);
-        for (let i = 0; i < rows; i++) {
+        for (let i = 0; i < squaresPerSide; i++) {
             const gridSquare = document.createElement("div");
             gridSquare.setAttribute("class", "square");
             rowDiv.append(gridSquare);
@@ -12,13 +16,13 @@ function createGrid(rows, columns) {
     }
 }
 
-createGrid(16, 16);
+promptBtn.addEventListener("click", createGrid);
 
 // listens for mouse entering squares
 container.addEventListener("mouseenter", e => {
     const targetClass = e.target.className;
     if(targetClass === "square") {
-        e.target.style.backgroundColor = "yellow";
+        e.target.style.backgroundColor = "grey";
     }
 }, true); // capture set to true as mouseenter event doesn't bubble for event delegation
 
