f63cc5a9f1148c2be8646a9ab2c3a1f2928d9ee6
nike-dev <146763214+nike-dev@users.noreply.github.com>
2025-03-21 17:54:16 +0200

    Add feature to remove previous grid
    Removes the previous grid everytime a new grid is
    defined.

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const container = document.querySelector("#container");
 const promptBtn = document.createElement("button");
+const gridContainer = document.createElement("div");
+gridContainer.setAttribute("id", "grid-container");
+
+
 promptBtn.textContent = "Start";
 container.append(promptBtn);
+container.append(gridContainer);
+
+// creates a grid of squares 
 function createGrid(e) {
     const squaresPerSide = prompt("What size grid you want?");
+    if (gridContainer.hasChildNodes()) {
+        while (gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.lastChild);
+        }
+    }
     for (let j = 0; j < squaresPerSide; j++) {
+
         const rowDiv = document.createElement("div");
         rowDiv.setAttribute("class", "rowDiv");
-        container.append(rowDiv);
+        gridContainer.append(rowDiv);
         for (let i = 0; i < squaresPerSide; i++) {
             const gridSquare = document.createElement("div");
             gridSquare.setAttribute("class", "square");
@@ -21,7 +34,7 @@ promptBtn.addEventListener("click", createGrid);
 // listens for mouse entering squares
 container.addEventListener("mouseenter", e => {
     const targetClass = e.target.className;
-    if(targetClass === "square") {
+    if (targetClass === "square") {
         e.target.style.backgroundColor = "grey";
     }
 }, true); // capture set to true as mouseenter event doesn't bubble for event delegation
